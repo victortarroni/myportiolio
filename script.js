b0f721a0f8ba3b5095ebfc62a37c52b5bb759e50
Victor Rabbi Tarroni Barbosa <victortarroni@Victors-MacBook-Air.local>
2023-11-10 11:50:06 +0000

    adjustments on height and width

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use stric";
 
 var icon = document.getElementById("icon");
-// callback -> function passa como argumento
+
 icon.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
-  // when we click on the icon, it will add or remove the dark theme to the body
+
   if (document.body.classList.contains("dark-theme")) {
     icon.src = "/images/white-vector.png";
   } else {
@@ -21,11 +21,11 @@ const respMenu = document.querySelector("#menu");
 
 const bttnClose = document.querySelector("#btn-close");
 
-const removeHidden = function () {
+const removeHidden = function() {
   respMenu.classList.remove("hidden-menu");
 };
 
-const closeMenu = function () {
+const closeMenu = function() {
   respMenu.classList.add("hidden-menu");
 };
 
@@ -42,7 +42,7 @@ const menuBarHeight = document
 const menu = document.querySelector("#menu");
 const header = document.querySelector("#header");
 
-const fixedMenu = function (entries) {
+const fixedMenu = function(entries) {
   const [entry] = entries;
   if (!entry.isIntersecting) {
     menu.classList.add("sticky");
@@ -62,12 +62,14 @@ menuObserver.observe(header);
 // Rolamento //
 document
   .querySelector(".menu-holder")
-  .addEventListener("click", function (elem) {
+  .addEventListener("click", function(elem) {
     elem.preventDefault();
     respMenu.classList.add("hidden-menu");
     if (elem.target.classList.contains("link")) {
       const id = elem.target.getAttribute("href");
-      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth"
+      });
     }
   });
 
@@ -75,18 +77,20 @@ document
 
 document
   .querySelector("#about-me-btn")
-  .addEventListener("click", function (elem) {
+  .addEventListener("click", function(elem) {
     console.log(elem);
     elem.preventDefault();
     if (elem.target.id) {
       const id = elem.target.getAttribute("href");
-      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth"
+      });
     }
   });
 
 // faded menu //
 
-const fadedmenu = function (e) {
+const fadedmenu = function(e) {
   if (e.target.classList.contains("link")) {
     const link = e.target;
     const brothers = link.closest("#menu").querySelectorAll(".link");
@@ -103,7 +107,7 @@ const fadedmenu = function (e) {
 
 document.querySelector("#menu").addEventListener("mouseover", fadedmenu);
 
-document.querySelector("#menu").addEventListener("mouseout", function (e) {
+document.querySelector("#menu").addEventListener("mouseout", function(e) {
   if (e.target.classList.contains("link")) {
     const link = e.target;
     const brothers = link.closest("#menu").querySelectorAll(".link");
@@ -120,7 +124,7 @@ document.querySelector("#menu").addEventListener("mouseout", function (e) {
 // revealing sections //
 const sections = document.querySelectorAll(".section");
 
-const reavealingSect = function (entries, observer) {
+const reavealingSect = function(entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
 
@@ -133,25 +137,19 @@ const sectionOberver = new IntersectionObserver(reavealingSect, {
   threshold: 0.4,
 });
 
-sections.forEach(function (section) {
+sections.forEach(function(section) {
   sectionOberver.observe(section);
   section.classList.add("section--hidden");
 });
 
-// Projects //
+// Projects section //
 
-// Contador para saber qual o slide atual
-// No click do next, precisa aumentar o contador e atualizar o left do container slides
-// Usar contador pra achar % pra por no left
-// Checar os limites mínimos e máximos para ciclar o contador
 
-// project solution//
-
-/* o addEventListener(load) aciona a funcao quando a pagina carrega. Esse è o padrão. */
+/* The addEventListener(load) triggers the function when the page loads. This is the default behavior. */
 window.addEventListener("load", () => {
   const slidesContainer = document.querySelector(".slides");
 
-  // criei essa variavel para pegar os numeros de slides. Ja que o queryselectorall retorna uma array, aqui a gente consegue tirar o numero de slides.
+  // I created this variable to fetch the number of slides. Since querySelectorAll returns an array, here we can retrieve the number of slides. //
   const slidesLength = document.querySelectorAll(".slide").length;
 
   // Buttons//
@@ -159,24 +157,24 @@ window.addEventListener("load", () => {
   const prevButton = document.querySelector(".slider-prev");
   const nextButton = document.querySelector(".slider-next");
 
-  // counter è a representacao numerica do slide atual//
+  // 'counter' is the numerical representation of the current slide. //
   let slide = 1;
 
-  // aqui è necessario mover o slide para a esquerda para que o proximo entre, por isso os valores nagtivos abaixo//
+  // Here, it's necessary to move the slide to the left so that the next one enters, hence the negative values below. //
 
   // 1: 0
   // 2: -100%
   // 3: -200%
 
-  // * / sempre resolve primeiro do que + - por isso, quando se tem uma some ou uma subtracao para ser calculada antes de uma multiplicacao ou divisap, se precisa a utilizacao de (). ex: `${ (slide - 1) * -100 }%`//
-  // O nome desses parênteses na matemática é "precedência"
+  // * always takes precedence over + and -, so when you have addition or subtraction to be calculated before multiplication or division, the use of parentheses is required. For example: `${(slide - 1) * -100}%` //
+  // The name for these parentheses in mathematics is "precedence."
 
   console.log("slidesLength", slidesLength);
 
   nextButton.addEventListener("click", () => {
     console.log(1);
     if (slide === slidesLength) {
-      /* return è para abortar a funçao*/
+      /* 'return' is to abort the function. */
       return;
     }
     slide++;
